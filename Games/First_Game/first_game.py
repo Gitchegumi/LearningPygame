1f58cd2cd4abca2bc7a16b400d3bf533a8e5d3eb
Mat <49793638+Gitchegumi@users.noreply.github.com>
2023-10-15 23:17:05 -0400

    Some assests and updates to the first game

--- Games/First_Game/first_game.py
+++ Games/First_Game/first_game.py
@@ -1,8 +1,49 @@
 '''This is my first attmept at making a game in python. It will most likely be a snake clone.'''
+import os
 import pygame
 from pygame.locals import * # pylint: disable=wildcard-import,unused-wildcard-import
 
+
 # pygame setup
 pygame.init() # pylint: disable=no-member
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((1000, 500))
 clock = pygame.time.Clock()
+background = pygame.image.load(os.path.join('Images', 'background.jpg')).convert()
+player = pygame.image.load(os.path.join('Images', 'player.jpg')).convert()
+RUNNING = True
+dt = 0 # pylint: disable=invalid-name
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+player_speed = 300 # pylint: disable=invalid-name
+
+while RUNNING:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: # pylint: disable=no-member
+            RUNNING = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.blit(background, (0,0), (0, 0, screen.get_width(), screen.get_height()))
+
+    screen.blit(player, player_pos)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]: # pylint: disable=no-member
+        player_pos.y -= player_speed * dt
+    if keys[pygame.K_s]: # pylint: disable=no-member
+        player_pos.y += player_speed * dt
+    if keys[pygame.K_a]: # pylint: disable=no-member
+        player_pos.x -= player_speed * dt
+    if keys[pygame.K_d]: # pylint: disable=no-member
+        player_pos.x += player_speed * dt
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # limits FPS to 120
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(120) / 1000
+
+pygame.quit() # pylint: disable=no-member
--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# Gitchegumi's jounrey through Pygame.
+# Gitchegumi's journey through Pygame.
 
 ## Current Progress
 - I have installed the Pygame module on my laptop and desktop and started to read through a tutorial on pygame.org.
 - I have started a shell for my first actual game that I will make on my own. A snake clone!
+- The shell, as of today 15 October, is an image of a little man moving around on a DnD map I had on hand. We're making progress!
 
 ## Tutorial
 > I have added example code for a bouncing ball as well as a solid color field and a moving circle.
