0c6b7341da6333a5a805e5b728f6792e357ce4e0
Mat <49793638+Gitchegumi@users.noreply.github.com>
2023-10-15 21:47:11 -0400

    A simple bouncing ball animation.

---
+++ Tutorials/Ball/bouncing_ball.py
@@ -0,0 +1,28 @@
+'''Importing sys and pygame libraries'''
+import sys
+import pygame
+pygame.init() # pylint: disable=no-member
+
+size = width, height = 1280, 720
+speed = [1, 1]
+black = 0, 0, 0
+
+screen = pygame.display.set_mode(size)
+
+ball = pygame.image.load("Tutorials\\Ball\\intro_ball.gif")
+ballrect = ball.get_rect()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: # pylint: disable=no-member
+            sys.exit()
+
+    ballrect = ballrect.move(speed)
+    if ballrect.left < 0 or ballrect.right > width:
+        speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > height:
+        speed[1] = -speed[1]
+
+    screen.fill(black)
+    screen.blit(ball, ballrect)
+    pygame.display.flip()
